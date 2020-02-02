82b507300de040c2d44aa0fcc8454e9cda6493a5
瀧野真由 <takino@mayu.local>
2020-02-02 14:39:28 +0900

    create mypage view

--- front/src/Component/UserShow.js
+++ front/src/Component/UserShow.js
@@ -1,57 +1,34 @@
 import React, { Component } from "react";
-import axios from "axios";
 import { Link } from "react-router-dom";
-import lscache from "lscache"
 import PostDestroy from "./PostDestroy";
 import NotificationSystem from 'react-notification-system'
 import { Card, CardTitle, CardBody, CardTime, CardLinkCenter, Button } from "../StyledComponent/Card";
-import { PageTitle, PageColor, PageText } from "../StyledComponent/Page";
+import { PageTitle, PageColor} from "../StyledComponent/Page";
 
 export default class UserShow extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      user: {},
-      posts: [],
-      token: lscache.get('token'),
-      mypage: false
-    };
     this.notificationSystem = React.createRef()
   }
   
   componentDidMount() {
-    const { match: { params } } = this.props;
-    const headers = {headers: {Authorization: `Bearer ${this.state.token}`}}
     const notification = this.notificationSystem.current
-    axios
-      .get(`http://localhost:3001/api/v1/users/${params.id}`, headers)
-      .then(response => {
-        this.setState({
-          user: response.data.user,
-          posts: response.data.posts,
-          mypage: response.data.mypage
-        });
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.log(error.response);
-      });
-    if (this.props.location.state != undefined) {
-      const message = this.props.location.state.message
-      const level = this.props.location.state.level
+    if (this.props.state != undefined) {
+      const message = this.props.state.message
+      const level = this.props.state.level
       notification.addNotification({
         message: message,
         level: level
       })
     }
   }
+
   render() {
     return (
       <PageColor>
         <NotificationSystem ref={this.notificationSystem} />
-        <PageTitle>{this.state.user.name}さんのページ</PageTitle>
-        <PageText>{this.state.user.created_at}に登録しました</PageText>
-        <PostList posts={this.state.posts} mypage={this.state.mypage} />
+        <PageTitle>{this.props.user.name}さんの投稿</PageTitle>
+        <PostList posts={this.props.posts} mypage={this.props.mypage} id={this.props.user.id} />
       </PageColor>
     );
   }
