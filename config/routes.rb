d302e3ebb403eae774504bf7683436907e1dbac5
瀧野真由 <takino@mayu.local>
2020-02-15 17:11:30 +0900

    delete namespace from rails-api

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,20 @@
 Rails.application.routes.draw do
-  root 'api/v1/users#index'
-  namespace 'api' do
-      namespace 'v1' do
-          resources :users do
-            member do
-              get :like
-            end
-          end
-          post '/login', to: 'login#login'
-          post '/logout', to: 'login#logout'
-          get '/me', to: 'users#me'
-          
-          resources :posts do
-            member do
-              get :liked
-            end
-          end
-
-          post '/like', to: 'likes#create'
-          post '/unlike', to: 'likes#destroy'
-      end
+  resources :users do
+    member do
+      get :like
+    end
   end
+  post '/login', to: 'login#login'
+  post '/logout', to: 'login#logout'
+  get '/me', to: 'users#me'
+          
+  resources :posts do
+    member do
+      get :liked
+    end
+  end
+
+  post '/like', to: 'likes#create'
+  post '/unlike', to: 'likes#destroy'
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
